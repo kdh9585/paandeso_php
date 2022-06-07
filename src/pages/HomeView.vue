a344a583fcdf8fd3221a6aca33edecfabf8ebe5d
kdh9585 <kdh9585@gmail.com>
2022-06-07 11:37:31 +0900

    last

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
   <h1>안녕하세요:)</h1>
+  <span class="badge badge-primary">Primary</span>
+  <span class="badge badge-secondary">Secondary</span>
+  <span class="badge badge-success">Success</span>
+  <span class="badge badge-danger">Danger</span>
+  <span class="badge badge-warning">Warning</span>
+  <span class="badge badge-info">Info</span>
+  <span class="badge badge-light">Light</span>
+  <span class="badge badge-dark">Dark</span>
 </template>
 
 <script>
-export default {
+  export default {
 
-}
+  }
 </script>
 
 <style>
-h1{
-  position: absolute;
-  left: 50%;
-  top:50%;
-  transform: translate(-50%, -50%);
-}
+  h1 {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
